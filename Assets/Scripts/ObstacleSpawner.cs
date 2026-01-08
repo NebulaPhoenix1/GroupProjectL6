d1eb1d3fa73933c5e68e59a48ac639e92c050aed
Luke <s267071@uos.ac.uk>
2026-01-08 17:24:52 +0000

    Tweaks to obstacle spawning for fixes

--- Assets/Scripts/ObstacleSpawner.cs
+++ Assets/Scripts/ObstacleSpawner.cs
@@ -12,10 +12,22 @@ public class ObstacleSpawner : ObjectSpawner
     protected override void Update()
     {
         base.Update(); //Calls base update in ObjectSpawner.cs
+        // Only check immediately after we try to spawn something
+        if (transform.childCount == 0 && prefabList.Length > 0)
+        {
+            //SpawnObject();
+        }
+        
     }
 
     protected override void Awake()
     {
         base.Awake(); //Calls base awake in ObjectSpawner.cs
+    }
+
+    public override void SpawnObject()
+    {
+        
+        base.SpawnObject();
     }
 }
