f30f04ff679648878c417923a8c55a398838b4cc
MLLacius <collinsleyton@gmail.com>
2026-01-12 00:24:13 +0000

    Added audiovisual cues to coin collection (particle and sound effects), WIP on changing audio from sequential coin collection

--- Assets/Scripts/CoinCollection.cs
+++ Assets/Scripts/CoinCollection.cs
@@ -3,7 +3,10 @@ using UnityEngine;
 public class CoinCollection : MonoBehaviour
 {
     [SerializeField] private GameMaster gameMaster;
+    [SerializeField] private AudioController audioController;
     private Collider coinCollider;
+    private Transform coinTransform;
+    [SerializeField] private ParticleSystem coinCollectEffect;
 
     private void OnTriggerEnter(Collider collision)
     {
@@ -12,15 +15,27 @@ public class CoinCollection : MonoBehaviour
         {
             gameMaster.IncrementCollectiblesGained();
         }
+
+        if (gameMaster.GetGameplayState())
+        {
+            audioController.PlayCoinCollect();
+            Instantiate(coinCollectEffect, new Vector3(this.gameObject.transform.position.x, this.gameObject.transform.position.y, this.gameObject.transform.position.z), Quaternion.identity);
+        }
         Destroy(gameObject);
     }
 
     void Awake()
     {
         coinCollider = GetComponent<Collider>();
+        coinTransform = GetComponent<Transform>();
         if(!gameMaster)
         {
             gameMaster = GameObject.Find("Game Master").GetComponent<GameMaster>();
+        }
+
+        if(!audioController)
+        {
+            audioController = GameObject.Find("AudioController").GetComponent<AudioController>();
         }
     }
 }
