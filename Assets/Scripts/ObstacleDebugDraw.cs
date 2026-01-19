a166ec6c1dc49c73ea466433894b85f69fc91b6c
Luke <luke.robb210@gmail.com>
2026-01-19 17:10:14 +0000

    Added a tool to show obstacle spawners in editor more easily

---
+++ Assets/Scripts/ObstacleDebugDraw.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class ObstacleDebugDraw : MonoBehaviour
+{
+    [Header("Box Settings")]
+    [SerializeField] private Color boxColor = Color.green;
+    [SerializeField] private Vector3 boxSize = new Vector3(2, 2, 2);
+    [SerializeField] private bool showOnlyWhenSelected = false;
+
+    // This draws the box always as long as Gizmos are enabled in the scene view
+    private void OnDrawGizmos()
+    {
+        if (showOnlyWhenSelected) return;
+        DrawBox();
+    }
+
+    // This draws the box only when you click on the object
+    private void OnDrawGizmosSelected()
+    {
+        if (showOnlyWhenSelected)
+        {
+            DrawBox();
+        }
+    }
+
+    private void DrawBox()
+    {
+        Gizmos.color = boxColor;
+        //Apply the object's rotation and position to the gizmo
+        Matrix4x4 rotationMatrix = Matrix4x4.TRS(transform.position, transform.rotation, transform.lossyScale);
+        Gizmos.matrix = rotationMatrix;
+        //Draw the wireframe box (lines only)
+        Gizmos.DrawWireCube(Vector3.zero, boxSize);
+    }
+}
