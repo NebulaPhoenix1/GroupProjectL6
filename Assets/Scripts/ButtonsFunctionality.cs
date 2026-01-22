f4cb0dd94bee407899d4e14910ff127d79da4439
Luke <luke.robb210@gmail.com>
2026-01-22 14:54:18 +0000

    i think everything is done
    Co-Authored-By: MLLacius <146335069+MLLacius@users.noreply.github.com>
    Co-Authored-By: Shara-Leah Butler <144057833+Shara-Leah@users.noreply.github.com>

--- Assets/Scripts/ButtonsFunctionality.cs
+++ Assets/Scripts/ButtonsFunctionality.cs
@@ -5,7 +5,7 @@ public class ButtonsFunctionality : MonoBehaviour
     // Play game button
     public void PlayGame()
     {
-        UnityEngine.SceneManagement.SceneManager.LoadScene("Shara's Scene");
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Main Game");
     }
 
     // Quit game button
